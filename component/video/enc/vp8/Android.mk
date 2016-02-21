b6c655bef2f0b9d73d94535479c07b72258f2e0b
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-21 15:33:27 +0100

    Build video codecs as 32bit libs only
    These libraries depend on libcsc, which has a dependency
    on libswconverter (which we can't build in 64bit right now).
    
    Change-Id: I4ca4dbca7c969126411135c4e920ca6dc611e9ba

--- component/video/enc/vp8/Android.mk
+++ component/video/enc/vp8/Android.mk
@@ -34,6 +34,8 @@ endif
 
 LOCAL_ARM_MODE := arm
 
+LOCAL_MULTILIB := 32
+
 LOCAL_STATIC_LIBRARIES := libExynosOMX_Venc libExynosOMX_OSAL libExynosOMX_Basecomponent \
 	libswconverter libExynosVideoApi
 LOCAL_SHARED_LIBRARIES := libc libdl libcutils libutils libui \
